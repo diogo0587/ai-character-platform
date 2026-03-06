bcea7dc8382ad208a776f2ec7ef004380983976f
Diogo0587 <11891583+diogo0587@users.noreply.github.com>
2026-03-05 23:04:04 -0300

    Criar lib/aiEngine.ts

---
+++ lib/aiEngine.ts
@@ -0,0 +1,23 @@
+export async function callAI(prompt){
+
+const response = await fetch(
+"https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct",
+{
+method:"POST",
+headers:{
+Authorization:`Bearer ${process.env.HF_TOKEN}`,
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+inputs:prompt,
+parameters:{
+max_new_tokens:200,
+temperature:0.9
+}
+})
+})
+
+const data = await response.json()
+
+return data[0]?.generated_text || "..."
+}
