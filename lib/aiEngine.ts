7560f825f6ac06aee051d7683e88d1c6838d2277
Diogo0587 <11891583+diogo0587@users.noreply.github.com>
2026-03-05 23:36:08 -0300

    Atualizar o aiEngine.ts

--- lib/aiEngine.ts
+++ lib/aiEngine.ts
@@ -1,24 +1,32 @@
 export async function callAI(prompt){
 
 const response = await fetch(
-"https://api-inference.huggingface.co/models/vicgalle/Roleplay-Llama-3-8B",
+"https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct",
 {
 method:"POST",
 headers:{
-Authorization:`Bearer ${process.env.HF_TOKEN}`,
+"Authorization": `Bearer ${process.env.HF_TOKEN}`,
 "Content-Type":"application/json"
 },
 body:JSON.stringify({
 inputs:prompt,
 parameters:{
-max_new_tokens:300,
-temperature:0.95,
-top_p:0.9
+max_new_tokens:200,
+temperature:0.9
 }
 })
 })
 
 const data = await response.json()
 
-return data[0]?.generated_text || "..."
+if(Array.isArray(data)){
+return data[0].generated_text
+}
+
+if(data.error){
+return "Erro da IA: " + data.error
+}
+
+return JSON.stringify(data)
+
 }
