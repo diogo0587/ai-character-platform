eede5544c8d5d4abf0573d854f97b2e5c4be5fe9
Diogo0587 <11891583+diogo0587@users.noreply.github.com>
2026-03-05 23:57:36 -0300

    Atualizar o aiEngine.ts

--- lib/aiEngine.ts
+++ lib/aiEngine.ts
@@ -1,11 +1,13 @@
 export async function callAI(prompt){
 
+try{
+
 const response = await fetch(
-"https://router.huggingface.co/hf-inference/models/HuggingFaceH4/zephyr-7b-beta",
+"https://router.huggingface.co/hf-inference/models/mistralai/Mistral-7B-Instruct-v0.2",
 {
 method:"POST",
 headers:{
-Authorization: `Bearer ${process.env.HF_TOKEN}`,
+"Authorization": `Bearer ${process.env.HF_TOKEN}`,
 "Content-Type":"application/json"
 },
 body:JSON.stringify({
@@ -19,23 +21,27 @@ temperature:0.9
 
 const text = await response.text()
 
-try{
+let data
 
-const data = JSON.parse(text)
+try{
+data = JSON.parse(text)
+}catch{
+return "Erro da API: " + text
+}
 
 if(Array.isArray(data)){
 return data[0].generated_text
 }
 
 if(data.error){
-return "Erro da IA: " + data.error
+return "Erro IA: " + data.error
 }
 
 return JSON.stringify(data)
 
-}catch{
+}catch(e){
 
-return "Erro da API: " + text
+return "Erro de conexão com IA"
 
 }
 
